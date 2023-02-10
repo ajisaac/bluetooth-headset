e91896b808dce3dae91698835263d3437ec528b2
Aaron Isaac <aaron@aisaac.co>
2023-02-10 01:01:15 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/*
+ Correctly find my bluetooth headset and connect automatically, also updating audio profile.
+ */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
